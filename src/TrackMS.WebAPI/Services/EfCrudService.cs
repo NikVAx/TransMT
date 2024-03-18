9084887310bb52c52b85e1997bc8bed6200d78a4
NikVAx <nik.vasilenko1203@gmail.com>
2024-03-18 23:02:29 +0300

    Add ICrudService interface and EfCrudService implementataion.

---
+++ src/TrackMS.WebAPI/Services/EfCrudService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrackMS.Data;
+using TrackMS.Domain.Abstractions;
+
+namespace TrackMS.WebAPI.Services;
+
+public class EfCrudService<TEntity, TKey> : ICrudService<TEntity, TKey> 
+    where TEntity : class, IEntity<TKey>
+{
+    private readonly DbSet<TEntity> _dataSet;
+    private readonly ApplicationDbContext _dbContext;
+
+    public EfCrudService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _dataSet = _dbContext.Set<TEntity>();
+    }
+
+    public virtual async Task CreateAsync(TEntity entity)
+    { 
+        _dataSet.Add(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public virtual async Task DeleteAsync(TEntity entity)
+    {
+        _dataSet.Remove(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public virtual async Task UpdateAsync(TEntity entity)
+    {
+        _dataSet.Remove(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public virtual async Task<TEntity> GetByIdAsync(TKey id)
+    {
+        var entity = await _dataSet
+            .FirstOrDefaultAsync(x => x.Id!.Equals(id));
+
+        if (entity == null)
+        {
+            throw new ApplicationException($"'{typeof(TEntity).Name}' not found");
+        }
+
+        return entity;
+    }
+
+}
