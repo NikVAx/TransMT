3d6dc6716271aa3132f1d08b76157605c12fec45
NikVAx <nik.vasilenko1203@gmail.com>
2024-03-20 23:22:25 +0300

    Add Session, UserRole and User models

---
+++ src/TrackMS.AuthService/Entities/Session.cs
@@ -0,0 +1,13 @@
+﻿namespace TrackMS.AuthService.Entities
+{
+    public class Session
+    {
+        public string SessionId { get; set; }
+        public string UserId { get; set; }
+        public string LogInIpAddress { get; set; }
+        public string RefreshToken { get; set; }
+        public bool IsBlocked { get; set; } = false;
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset ExpiredAt { get; set; }
+    }
+}
