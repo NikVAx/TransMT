0c2f676741a5373e60482ea5f044fab573397e0e
NikVAx <nik.vasilenko1203@gmail.com>
2024-05-21 17:08:15 +0300

    fix: geozone latlng; geature: type field to geozone (add unstaged files)

--- src/TrackMS.WebAPI/Features/GeoZones/DTO/CreateGeoZoneDto.cs
+++ src/TrackMS.WebAPI/Features/GeoZones/DTO/CreateGeoZoneDto.cs
@@ -6,6 +6,7 @@ public class CreateGeoZoneDto
 {
     public string Name { get; set; }
     public string Color { get; set; }
+    public string Type { get; set; }
 
     public ICollection<GeoPoint> Points { get; set; }
 }
